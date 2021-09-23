08a1a1574caee0cc46f851767ae4b9e0fd170448
Ian Yamashita <ianyamashita@Ians-MacBook-Air.local>
2021-09-22 20:38:19 -1000

    login and register refactored

--- client/src/components/Pages/About.js
+++ client/src/components/Pages/About.js
@@ -3,7 +3,8 @@ import React from 'react'
 const About = () => {
     return (
         <div>
-            <h1>This is about page</h1>
+            <h1>About Us</h1>
+            <p>3 guys</p>
         </div>
     )
 }
--- client/src/components/Pages/Login.js
+++ client/src/components/Pages/Login.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
 
+    const [input, setInput] = useState({
+        username: '',
+        password: ''
+    })
+
+    let changeHandler = (event) => {
+        setInput({ 
+            value: event.target,
+        })
+        
+    }
+
+
     return (
         <div>
-            <h1>This is login</h1>
+            <h1 className="text-center">Login</h1>
+            
+                <form className="text-center border border-light p-5" action="" method="POST">
+                    <div className="form-group">
+                    <label>
+                        Username:
+                        <input type="text" className="form-control" name="username"></input>
+                    </label>  
+                    </div>
+                    
+                    <div className="form-group">
+                    <label>
+                        Password:
+                        <input type="text" className="form-control" name="password"></input>
+                    </label>  
+                    </div>
+
+                    <div>
+                        <button>Submit</button>
+                    </div>
+
+                </form>
+            
         </div>
     )
 }
